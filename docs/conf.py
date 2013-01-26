403c19ba5cb17ac1b1da53e245e6bb43a44650c5
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2013-01-26 12:27:31 +0100

    missed one 2012 occurence here or there
    Signed-off-by: Kwpolska <kwpolska@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'TEMPLATE'
-copyright = u'2012, Kwpolska'
+copyright = u'2013, Kwpolska'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
