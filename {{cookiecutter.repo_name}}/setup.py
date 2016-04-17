b8146e4147267b2dbbe41299afd16de6e5cf0ea2
Chris Warrick <kwpolska@gmail.com>
2016-04-17 19:04:04 +0200

    Better setup.py and setup.cfg
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -2,23 +2,8 @@
 # -*- encoding: utf-8 -*-
 import sys
 import io
-from setuptools import setup
-from setuptools.command.test import test as TestCommand
+from setuptools import setup, find_packages
 
-with open('requirements.txt', 'r') as fh:
-    dependencies = [l.strip() for l in fh]
-
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = ['tests/']
-        self.test_suite = True
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.test_args)
-        sys.exit(errno)
 
 setup(name='{{ cookiecutter.repo_name }}',
       version='{{ cookiecutter.version }}',
@@ -31,7 +16,6 @@ setup(name='{{ cookiecutter.repo_name }}',
       long_description=io.open('./docs/README.rst', 'r', encoding='utf-8').read(),
       platforms='any',
       zip_safe=False,
-      cmdclass={'test': PyTest},
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=['Development Status :: 1 - Planning',
                    'Programming Language :: Python',
@@ -39,14 +23,15 @@ setup(name='{{ cookiecutter.repo_name }}',
                    'Programming Language :: Python :: 2.7',
                    'Programming Language :: Python :: 3',
                    'Programming Language :: Python :: 3.3',
-                   'Programming Language :: Python :: 3.4'],
-      packages=['{{ cookiecutter.repo_name }}'],
-      install_requires=dependencies,
-      #requires=['stuff'],
-      #data_files=[('file', ['dest']),],
-      #entry_points={
-          #'console_scripts': [
-              #'{{ cookiecutter.repo_name }} = {{ cookiecutter.repo_name }}.__main__:main',
-          #]
-      #},
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5'],
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=[],
+      # data_files=[('file', ['dest']),],
+      # entry_points={
+           #'console_scripts': [
+               #'{{ cookiecutter.repo_name }} = {{ cookiecutter.repo_name }}.__main__:main',
+           #]
+      # },
       )
